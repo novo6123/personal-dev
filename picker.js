bd8e7e7d747388725912ddda9bb25799e7027822
Norman Vo <norman.vo@gmail.com>
2015-06-05 15:54:56 +1000

    picker script update - not great

--- picker.js
+++ picker.js
@@ -1,29 +1,47 @@
-// var temp = [1,2,3,4,5,6,7,8,9];
-// pick(3) // expect [1,5,9]
-// pick(4) // expect [1,3,6,9]
-// pick(5) // expect [1,3,5,7,9]
-
-var temp = [1,2,3,4,5,6,7,8,9],
+/*
+ * @desc still not doing what I want!
+ */
+var temp = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+    temp2 = [1,2,3,4,5,6,7,8,9],
 
     picker = function (inputArray, numOfPoints) {
-        var i = 0,
+        var i = 1,
+            tempArray = [],
             subArray = [],
-            dataLength = inputArray.length;
+            dataLength = 0,
+            point = 0,
+            pointsToDistribute = 0;
 
         // first item
         subArray.push(inputArray[0]);
 
-        // other points distributed
-        for (; i < dataLength; i+=1) {
-            // unfinished
-            // subArray.push
+        tempArray = inputArray.slice(1,this.length - 1);
+        dataLength = tempArray.length;
+
+        console.log("tempArray", tempArray);
+
+        // numOfPoints - firstItem - lastItem
+        pointsToDistribute = numOfPoints - 2;
+        console.log("pointsToDistribute", pointsToDistribute);
+
+        var interval = Math.floor(dataLength / pointsToDistribute);
+
+        console.log("interval", interval);
+
+
+        for (; i <= pointsToDistribute; i+=1) {
+
+            point = tempArray[i*interval-1];
+            console.log("point",point);
+
+            subArray.push(point);
         }
 
         // last item
-        subArray.push(inputArray[dataLength-1]);
+        subArray.push(inputArray[inputArray.length-1]);
 
         return subArray;
     };
 
 
-picker(temp, 3);
+picker(temp, 5);
