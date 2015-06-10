9a63a48bfe50621fbb721a8785b0e13c317e13e1
Norman Vo <norman.vo@gmail.com>
2015-06-10 17:43:19 +1000

    added javascripting solutions
    added javascripting solutions

---
+++ javascripting/07-rounding-numbers.js
@@ -0,0 +1,5 @@
+var roundUp = 1.5,
+    rounded = 0;
+
+rounded = Math.round(roundUp);
+console.log(rounded);
---
+++ javascripting/10-for-loop.js
@@ -0,0 +1,8 @@
+var total = 0,
+    limit = 10,
+    i = 0;
+
+for (; i < limit; i+=1) {
+    total += i;
+}
+console.log(total);
