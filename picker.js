0d11d4866ed5d10c2f3d853b60bb27b3c26d33d4
Norman Vo <norman.vo@gmail.com>
2015-06-10 10:42:09 +1000

    script improvements
    removed unused subdiv condition;
    added debugMsg as function;
    removed hardcoded array;
    added array maker;
    updated documentation

--- picker.js
+++ picker.js
@@ -1,19 +1,38 @@
 /*
  * @auth twitter.com/mrnovo6123 (norman.vo@gmail.com)
  * @desc for any passed in array, picked an evenly distributed set of points
+ * @example picker.pick(picker.makeArray(100),20);
+ *
+ * TODO: add offset to first point depending on subdivisions
  */
-var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
-    arrayn = (function() {
-        var i = 0,
-            sub = [];
+var picker = {
 
-            for (; i<100; i+=1) {
+        /*
+         * @desc generate array of numbers up to limit
+         */
+        "makeArray": function (lim) {
+            var i = 0,
+                sub = [];
+
+            for (; i<lim; i+=1) {
                 sub.push(i);
             }
             return sub;
-    }());
+        },
 
-    picker = {
+
+        /*
+         * @desc output debug message
+         */
+        "debugMsg": function (debug, arguments) {
+            if (!!debug) {
+                if (console) {
+                    console.log(arguments);
+                }
+            }
+        },
+
+
         "pick": function(inputArray, num, debug) {
             "use strict";
 
@@ -21,28 +40,22 @@ var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,2
                 subArray = [],
                 i = 1;
 
-            if (!!debug) {
-                console.log("array length: ", len);
-            }
+            picker.debugMsg(debug, "array length: ", len);
 
             for (; i <= num; i+=1) {
                 var iter = i - 1,
                     subdiv = Math.floor(len / (num - 1)),
                     ptIndex = 0;
 
-                if (subdiv < 0) {
-                    console.log("subdiv < 0");
-                    subdiv = 0;
-                }
 
+                // TO DO: add first point offset??
                 ptIndex = subdiv * iter;
 
 
-                if (!!debug) {
-                    console.log("iter", iter);
-                    console.log("subdiv", subdiv);
-                    console.log("inputArray[subdiv * iter]", inputArray[ptIndex]);
-                }
+                picker.debugMsg(debug, "iter", iter);
+                picker.debugMsg(debug, "subdiv", subdiv);
+                picker.debugMsg(debug, "inputArray[subdiv * iter]", inputArray[ptIndex]);
+
 
                 if (ptIndex >= len) {
 
@@ -59,6 +72,4 @@ var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,2
         }
     };
 
-
-// picker.pick(arrayn, 3); // [0, 50, 99]; // correct
-// picker.pick(arrayn, 4); // [0, 33, 66, 99]; // correct
+// picker.pick(picker.makeArray(100),30,true);
