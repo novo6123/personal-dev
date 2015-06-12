dcbaaaf83c54214891321fbc144ef860619bbf4e
Norman Vo <norman.vo@gmail.com>
2015-06-12 14:48:31 +1000

    google search XHR fiddle
    functional, but incomplete idea

---
+++ js/google-query.js
@@ -0,0 +1,29 @@
+// query Google AU from the browser console!
+// returned object needs to be parsed...
+// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+
+var gq = {
+    "getRequest": function (queryString) {
+        "use strict";
+
+        var req = new XMLHttpRequest(),
+            target = "https://www.google.com.au/search?num=20&safe=off&q=";
+
+        req.onload = (function (d) {
+            console.log(d.currentTarget.responseText);
+        });
+
+        req.open("get", target+queryString, true);
+        req.send();
+    },
+
+    "query": function (queryString) {
+        "use strict";
+
+        this.getRequest(queryString);
+        return this;
+    }
+};
+
+// gq.query("hello world");
+// I dont know what to do with this...
